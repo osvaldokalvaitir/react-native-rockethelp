dee614d2d5140ef8315eb8274bfae73cc0b1b20a
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2022-07-18 21:13:42 -0300

    feature: installation and configuration of native base

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,11 @@
-import { View, Text } from 'react-native';
+import { VStack, Heading } from 'native-base';
 
 export function SignIn(){
   return(
-    <View>
-      <Text>Olá</Text>
-    </View>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+    </VStack>
   )
 }
