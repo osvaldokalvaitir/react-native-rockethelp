c39b09cc1770cee2fa48a9871c09d6e6eadeb518
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2022-07-19 21:10:44 -0300

    chore: adds inputs in signin

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,13 @@
-import { VStack, Heading } from 'native-base';
+import { VStack, Heading, Icon, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_primary.svg';
 
+import { Input } from '../components/Input';
+
 export function SignIn() {
+  const { colors } = useTheme();
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
@@ -10,6 +15,17 @@ export function SignIn() {
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acesse sua conta
       </Heading>
+
+      <Input
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+      />
+
+      <Input placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+      />
 
     </VStack>
   )
