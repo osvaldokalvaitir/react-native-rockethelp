cc7bd7ad9ee6be3785c1b1dbb86a82d52d524eef
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2022-07-24 12:25:16 -0300

    chore: adds authentication in signin

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,32 @@
+import { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
+import { Loading } from '../components/Loading';
 import { SignIn } from '../screens/SignIn';
+
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(response => {
+      setUser(response);
+      setIsLoading(false);
+    })
+
+    return subscriber;
+  }, []);
+
+  if(isLoading) {
+    return <Loading />
+  }
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   )
 }
